d509f69203c290df5603a29eafb6bbb964b3efbb
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-20 13:46:23 +0300

    improve styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex flex-wrap items-center justify-center gap-7 bg-white'>
+    <div className='flex flex-wrap items-end justify-center gap-7 bg-white'>
         <div className='w-100 grid grid-cols-2 h-80 items-end'>
             <div className=' p-7 flex flex-col justify-around items-center'>
                 <h1 className='text-3xl'>WOODEN MINIMALISTC CHAIRS</h1>
@@ -11,7 +11,7 @@ const Hero = () => {
             </div>
             <img src="imgs/chair.png" alt="" />
         </div>
-        <div className='w-100 flex flex-col mt-4 items-center h-80'>
+        <div className='w-100 flex flex-col mt-4 items-center justify-end h-80'>
             <h1 >PACK 5 T-SHIRTS FOR SUMMER</h1>
             <h3 className='my-3'>Starting at <span className='text-orange-600'>$99.99</span></h3>
             <img src="/imgs/tShirts.png" alt="" />
