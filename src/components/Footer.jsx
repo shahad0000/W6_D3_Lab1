503642ef4e80e30b154e2463d773b78bfdcb00f2
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-20 13:43:13 +0300

    improve styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-around items-center mt-3 flex-wrap bg-white">
-      <div className="w-70 flex flex-col">
+    <div className="flex justify-around items-center mt-6 flex-wrap bg-white">
+      <div className="w-70 flex flex-col m-4">
         <div>BASE BLUETOOTH WIRELESS HEADPHONES</div>
         <div>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
@@ -12,7 +12,7 @@ const Footer = () => {
       </div>
       <div className="w-70">
         <div className="text-green-700 text-4xl">$219.44</div>
-        <button className="bg-amber-300 text-4xl border p-2 border-gray-700 mt-4">Shop Now</button>
+        <button className="bg-amber-400 text-4xl rounded-lg	 p-2 border-gray-700 mt-4">Shop Now</button>
       </div>
       <div className="w-80">
         <img className="h-50" src="imgs/headPhones.png" alt="" />
